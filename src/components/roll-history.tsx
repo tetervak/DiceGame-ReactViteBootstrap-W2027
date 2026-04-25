a0b401c2b760f14f549a5e163953e864d07ab9be
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-25 00:22:19 -0400

    formatted the history to use a table

--- src/components/roll-history.tsx
+++ src/components/roll-history.tsx
@@ -1,5 +1,5 @@
 import {useHistory} from "../hooks/use-history.ts";
-import {Button} from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
 import {XLg} from "react-bootstrap-icons";
 
 export function RollHistory(){
@@ -13,11 +13,26 @@ export function RollHistory(){
     return(
         <>
             <h2 className="mt-4 mb-3">Roll History</h2>
-            <ol>
-                {historyData.items.map(item => (
-                    <li key={item.id}>{item.rollData.values.join(' + ')}&nbsp;=&nbsp;{item.rollData.total}</li>
+            <Table striped bordered className="w-auto text-center">
+                <thead>
+                    <tr className="table-primary">
+                        <th>Count</th>
+                        <th>Dice Values</th>
+                        <th>Total</th>
+                        <th>Timestamp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {historyData.items.map((item, index) => (
+                    <tr key={item.id}>
+                        <td>{index + 1}</td>
+                        <td>{item.rollData.values.join(' + ')}</td>
+                        <td>{item.rollData.total}</td>
+                        <td>{new Date(item.timestamp).toLocaleString('en-CA')}</td>
+                    </tr>
                 ))}
-            </ol>
+                </tbody>
+            </Table>
             <Button onClick={clearHistory} variant="outline-danger ms-3">Clear History<XLg className="ms-2"/></Button>
         </>
     )
