bc8ae793b2cc179d9c62de8f6f7bf4ddb03e9754
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-24 21:44:57 -0400

    defined dice roller component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import * as React from "react";
 import {Footer} from "./components/footer.tsx";
 import {Header} from "./components/header.tsx";
-import {RollResult} from "./components/roll-result.tsx";
-import {useDice} from "./hooks/use-dice.ts";
-import {Button, Col, Container, Row, Stack} from "react-bootstrap";
-import {Form} from "react-bootstrap";
-import {CheckLg, XLg} from "react-bootstrap-icons";
+import {DiceRoller} from "./components/dice-roller.tsx";
 
 function App() {
-
-    const {rollData, rollDice, reset, numberOfDice, setNumberOfDice} = useDice();
-
-    const onNumberOfDiceChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-        setNumberOfDice(parseInt(event.target.value))
-    }
-
     return (
         <>
             <Header title="Dice Game"/>
-            <Container>
-                <h2 className="mt-4 mb-3">Roll the Dice</h2>
-                {rollData && <RollResult data={rollData}/>}
-                <Row className="align-items-baseline my-4">
-                    <Col xs="auto">
-                        <Form.Label>Number of Dice:</Form.Label>
-                    </Col>
-                    <Col xs="auto">
-                        <Form.Select value={numberOfDice} onChange={onNumberOfDiceChange}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </Form.Select>
-                    </Col>
-                </Row>
-                <Stack direction="horizontal" gap={2} className="mb-3 ms-4">
-                    <Button onClick={rollDice} variant="primary">Roll Dice<CheckLg className="ms-2"/></Button>
-                    <Button onClick={reset} variant="outline-warning">Reset<XLg className="ms-2"/></Button>
-                </Stack>
-            </Container>
+            <DiceRoller/>
             <Footer name="Alex Tetervak"/>
         </>
     )
---
+++ src/components/dice-roller.tsx
@@ -0,0 +1,40 @@
+import {useDice} from "../hooks/use-dice.ts";
+import * as React from "react";
+import {RollResult} from "./roll-result.tsx";
+import {Button, Col, Container, Form, Row, Stack} from "react-bootstrap";
+import {CheckLg, XLg} from "react-bootstrap-icons";
+
+export function DiceRoller() {
+
+    const {rollData, rollDice, reset, numberOfDice, setNumberOfDice} = useDice();
+
+    const onNumberOfDiceChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+        setNumberOfDice(parseInt(event.target.value))
+    }
+
+    return (
+        <Container>
+            <h2 className="mt-4 mb-3">Roll the Dice</h2>
+            {rollData && <RollResult data={rollData}/>}
+            <Row className="align-items-baseline my-4">
+                <Col xs="auto">
+                    <Form.Label>Number of Dice:</Form.Label>
+                </Col>
+                <Col xs="auto">
+                    <Form.Select value={numberOfDice} onChange={onNumberOfDiceChange}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </Form.Select>
+                </Col>
+            </Row>
+            <Stack direction="horizontal" gap={2} className="mb-3 ms-4">
+                <Button onClick={rollDice} variant="primary">Roll Dice<CheckLg className="ms-2"/></Button>
+                <Button onClick={reset} variant="outline-warning">Reset<XLg className="ms-2"/></Button>
+            </Stack>
+        </Container>
+    )
+
+}
